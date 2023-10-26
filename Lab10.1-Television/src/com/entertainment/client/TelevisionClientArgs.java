4c3196a1fbe3284de72631c901bf5a7cd7655422
Lawliet <Vitalii.Chibizov@tlgcohort.com>
2023-10-26 12:54:31 -0700

    Lab10.1-Television - mess in args (need to fix from original)

---
+++ Lab10.1-Television/src/com/entertainment/client/TelevisionClientArgs.java
@@ -0,0 +1,45 @@
+package com.entertainment.client;/*
+ * This code is sample code, provided as-is, and we make no
+ * warranties as to its correctness or suitability for any purpose.
+ *
+ * We hope that it's useful to you.  Enjoy.
+ * Copyright LearningPatterns Inc.
+ */
+
+import src.com.entertainment.Television;
+
+/**
+ * Intended usage (by human):
+ *  $ java TelevisionClientArgs <brand> <volume> <display>
+ *
+ * Example:
+ *  $ java TelevisionClientArgs Samsung 32 PLASMA
+ *
+ * GOAL:
+ *  Create an instance of Television with the specifications (values) provided by the user.
+ */
+class TelevisionClientArgs {
+
+    public static void main(String[] args) {
+        // must first check for the presence of your (required) 3 arguments
+        if (args.length < 3) {
+            printUsage();
+            return;  // early return from main(), application exits
+        }
+
+        // at this point, you can safely proceed, because you got your arguments
+        // first, let's just show that we got them
+        System.out.println("You provided " + args.length + " arguments");
+
+    //    Television tv = new Television(brand, volume, display);
+    }
+
+    private static void printUsage() {
+        String usage = "Usage: java TelevisionClientArgs <brand> <volume> <display>";
+        String example = "Example: java TelevisionClientArgs Samsung 32 PLASMA";
+        String note = "Note: supported displays are LED, OLED, LCD, CRT, PLASMA";
+        System.out.println(usage);
+        System.out.println(example);
+        System.out.println(note);
+    }
+}
