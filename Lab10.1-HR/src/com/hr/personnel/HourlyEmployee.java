a7877e5a895d717ccecd039d1f1ec7d0e34faf16
Lawliet <Vitalii.Chibizov@tlgcohort.com>
2023-10-26 14:14:40 -0700

    Lab10.1-Extra complete (argExce > wageExce + try / catch)

--- Lab10.1-HR/src/com/hr/personnel/HourlyEmployee.java
+++ Lab10.1-HR/src/com/hr/personnel/HourlyEmployee.java
@@ -8,6 +8,8 @@
 
 package com.hr.personnel;
 
+import gov.irs.IllegalWageException;
+
 import java.time.LocalDate;
 
 public class HourlyEmployee extends Employee {
@@ -25,7 +27,7 @@ public class HourlyEmployee extends Employee {
     }
 
     public HourlyEmployee(String name, LocalDate hireDate, double rate, double hours)
-    throws IllegalArgumentException {
+    throws IllegalWageException {
         this(name, hireDate);   // delegate to neighboring ctor for name, hireDate
         setRate(rate);          // handle rate here, by delegating to setter
         setHours(hours);        // handle hours here, by delegating to setter
@@ -48,10 +50,10 @@ public class HourlyEmployee extends Employee {
         return rate;
     }
 
-    public void setRate(double rate) throws IllegalArgumentException{
+    public void setRate(double rate) throws IllegalWageException{
         if (rate > FEDERAL_MINIMUM_WAGE) {
             this.rate = rate;
-        } else throw new IllegalArgumentException
+        } else throw new IllegalWageException
                 ( String.format("Illegal wage: %s. Federal minimum wage is: %s", rate, FEDERAL_MINIMUM_WAGE));
     }
 
