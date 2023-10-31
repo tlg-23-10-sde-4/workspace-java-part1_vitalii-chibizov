f159b82dcfd58f3c149573343e3a6a89f87b7181
Lawliet <Vitalii.Chibizov@tlgcohort.com>
2023-10-31 09:26:46 -0700

    Lab12.1 (read / write) best unvalid haiku is written

--- Lab12.1/src/com/poetry/io/PoemClient.java
+++ Lab12.1/src/com/poetry/io/PoemClient.java
@@ -8,8 +8,7 @@
 
 package com.poetry.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 
 public class PoemClient {
 
@@ -18,7 +17,7 @@ public class PoemClient {
      */
     public static void main(String[] args) {
         // readPoem();
-        // writePoem();
+         writePoem();
     }
 
     /**
@@ -35,8 +34,11 @@ public class PoemClient {
      */
     private static void readPoem() {
         // TODO: initialize 'reader' variable and complete the try block
-        try (BufferedReader reader = null) {
-
+        try (BufferedReader reader = new BufferedReader(new FileReader("famous-poem.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {       // null means EOF
+                System.out.println(line);
+            }
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -55,6 +57,17 @@ public class PoemClient {
      * Use a try-with-resources to initialize the stream and auto-close it.
      */
     private static void writePoem() {
-        // TODO
-    }
+            try (PrintWriter writer = new PrintWriter(new FileWriter("My haiku.txt"))) {
+                writer.println("Darkness in my mind");
+                writer.println("We all pretend, we understand");
+                writer.println("But effort worthless");
+                /*System.out.println("" +
+                        "Darkness in my mind\n" +
+                        "We all pretend, we understand\n" +
+                        "But effort worthless");*/
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            }
 }
